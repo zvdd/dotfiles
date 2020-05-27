0298fb7c9f5cb18ce0ddde7edcb4b4e21389df3b
Brenden Durham <zvddacc@gmail.com>
2020-05-26 22:36:08 -0500

    vim
    setup, vimrc, vim_plug, vim_base, vsvimrc, ideavimrc

---
+++ vim/setup.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR" || return 1
+
+loginfo()  { printf "%b[info]%b %s\n"  '\e[0;32m\033[1m' '\e[0m' "$@" >&2; }
+logwarn()  { printf "%b[warn]%b %s\n"  '\e[0;33m\033[1m' '\e[0m' "$@" >&2; }
+logerror() { printf "%b[error]%b %s\n" '\e[0;31m\033[1m' '\e[0m' "$@" >&2; }
+
+# https://github.com/neovim/neovim/wiki/Installing-Neovim
+# install_neovim() {
+#     hash nvim &>/dev/null && return
+#     if hash apt 2>/dev/null; then
+#         if ! (sudo apt update && sudo apt install neovim -y); then
+#             loginfo "failed installing neovim from package manager. try nvim.appimage instead..."
+#             mkdir -p ~/bin
+#             curl -fL -o /tmp/nvim \
+#                 https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+#             chmod u+x /tmp/nvim
+#             mv /tmp/nvim ~/bin/nvim
+#         fi
+#     elif hash pacman 2>/dev/null; then
+#         sudo pacman --noconfirm -Sy neovim
+#     elif hash brew 2>/dev/null; then
+#         sudo brew install neovim
+#     fi
+# }
+
+
+install_configs() {
+    mkdir -p ~/.config ~/.vim/spell
+
+    ln -snf "$HOME/.vim"               ~/.config/nvim
+    ln -sf  "$SDIR/vimrc"              ~/.config/nvim/init.vim
+    ln -sf  "$SDIR/vimrc"              ~/.vimrc
+    # ln -sf  "$SDIR/gvimrc"             ~/.gvimrc
+    ln -sf  "$SDIR/vim_map"            ~/.vim_map
+    ln -sf  "$SDIR/vim_base"           ~/.vim_base
+    ln -sf  "$SDIR/vim_plug"           ~/.vim_plug
+    # ln -sf  "$SDIR/spell/en.utf-8.add" ~/.vim/spell/en.utf-8.add
+    # ln -snf "$SDIR/UltiSnips"          ~/.vim/UltiSnips
+    # ln -sf  "$SDIR/coc-settings.json"  ~/.vim/coc-settings.json
+    touch ~/.vim_local
+
+    ln -sf  "$SDIR/ideavimrc" ~/.ideavimrc
+    ln -sf  "$SDIR/vsvimrc" ~/.vsvimrc
+
+    # https://github.com/onivim/oni
+    hash oni &>/dev/null && {
+        mkdir -p ~/.config/oni
+        ln -sf "$SDIR/oni.tsx" ~/.config/oni/config.tsx
+    }
+}
+
+# install_plugins() {
+#     local plug_manager=~/.vim/autoload/plug.vim
+#     if ! [ -f "$plug_manager" ]; then
+#         mkdir -p ~/.vim/autoload
+#         curl -fLo "$plug_manager" https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim ||
+#             git clone --depth=1 https://github.com/junegunn/vim-plug ~/.vim/vim-plug && ln -sf ~/.vim/vim-plug/plug.vim "$plug_manager"
+#     fi
+#     if ! hash node &>/dev/null; then
+#         loginfo "install node(coc.nvim need it)..."
+#         mkdir -p ~/.local
+#         curl -fsSL https://install-node.now.sh/latest | bash -s -- --prefix="$HOME/.local" -y
+#     fi
+#     loginfo "install vim plugins..."
+#     nvim +PlugInstall +qall
+
+#     # gtags
+#     ln -sf "$SDIR/gtags.conf" ~/.gtags.conf
+# }
+
+
+
+# loginfo "install neovim..."
+# install_neovim
+
+loginfo "install configs..."
+install_configs
+
+# loginfo "install plugins..."
+# install_plugins
