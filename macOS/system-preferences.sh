157d4239d92a805c2ab16351f4934a9ca9dbb058
Brenden Durham <paybdd@gmail.com>
2021-04-03 11:48:16 -0500

    spacemacs, goku

--- macOS/system-preferences.sh
+++ macOS/system-preferences.sh
@@ -225,7 +225,7 @@ sudo pmset -a sms 0
 sudo pmset -a standbydelay 86400
 
 # Remove the sleep image file to save disk space
-# sudo rm /private/var/vm/sleepimage
+#sudo rm /private/var/vm/sleepimage
 # Create a zero-byte file instead…
 #sudo touch /private/var/vm/sleepimage
 # …and make sure it can’t be rewritten
