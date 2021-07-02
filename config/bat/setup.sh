5f49253fbdcb4ab16dbe7c89753bc1225020fb63
Brenden Durham <paybdd@gmail.com>
2021-07-01 21:51:39 -0500

    add bat

---
+++ config/bat/setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+################################################################################
+#    Author: Wenxuan                                                           #
+#     Email: wenxuangm@gmail.com                                               #
+#   Created: 2019-01-18 18:04                                                  #
+################################################################################
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || exit 1
+
+mkdir -p ~/.config/bat
+ln -sf "$SCRIPT_DIR/config" ~/.config/bat/config
+
+# Themes
+# mkdir -p "$(bat --config-dir)/themes"
+# cd "$(bat --config-dir)/themes" || exit 1
+# test ! -d gruvbox &&
+    # git clone --depth=1 --branch 3.0.0 https://github.com/Briles/gruvbox.git
+# bat cache --build
